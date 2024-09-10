ee10dd9f4d58eeb8ac312f179a7785094a8820f1
Rystzov <rystzov1995@yandex.ru>
2024-09-10 15:49:25 +0300

    Up cacl

---
+++ brain_games/games/calc.py
@@ -0,0 +1,21 @@
+from random import randint, choice
+
+DESCRIPTION = 'What is the result of the expression?'
+START = 0
+END = 10
+
+def generate_question_and_answer():
+    
+    sign = choice(['+','-','*'])
+    number1 = randint(START, END)
+    number2 = randint(START, END)
+    question = f'{number1}{sign}{number2}'
+    
+    if sign == '+':
+        correct_answer = number1 + number2
+    elif sign == '-':
+        correct_answer = number1 - number2
+    else:
+        correct_answer = number1 * number2
+    
+    return question, str(correct_answer)
