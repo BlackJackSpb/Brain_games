003a4b1e35600ef1c1d5c3e9a4775b718c060127
Rystzov <rystzov1995@yandex.ru>
2024-09-12 10:38:43 +0300

    up1

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,15 +4,16 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def generate_question_and_answer():
-
-    def prime(x):
-
-        for i in range(2, int(x * 0,5) + 1):
-            if x % i == 0:
-                return False
-        return True
-
     number = randint(1, 100)
     question = f'Question: {number}'
-    correct_answer = 'yes' if prime(number) else 'no'
+    correct_answer = 'yes' if is_prime(number) else 'no'
     return question, correct_answer
+
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
