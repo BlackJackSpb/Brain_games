6309fcefc6ec3768a4d34631363c6a8881e2efa3
Rystzov <rystzov1995@yandex.ru>
2024-09-16 11:44:24 +0300

    progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from brain_games.engine import play
-from brain_games.games import prog
+from brain_games.games import progression
 
 
 def main():
-    play(prog)
+    play(progression)
 
 
 if __name__ == '__main__':
