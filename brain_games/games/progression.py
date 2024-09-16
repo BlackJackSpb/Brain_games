b9028d82c8c0d6c8472958207ccb6934fe5897cb
Rystzov <rystzov1995@yandex.ru>
2024-09-16 12:16:16 +0300

    progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,8 +5,8 @@ START_STEP = 1
 END_STEP = 10
 START = 1
 END = 100
-START_SIZE = 4
-END_SIZE = 8
+START_SIZE = 5
+END_SIZE = 10
 
 
 def generate_question_and_answer():
@@ -14,12 +14,12 @@ def generate_question_and_answer():
     progression_size = randint(START_SIZE, END_SIZE)
     start = randint(START, END)
     step = randint(START_STEP, END_STEP)
-    progression = [start]
-    correct = randint(0, step - 1)
+    progression = []
+    correct = randint(0, progression_size - 1)
 
     for _ in range(progression_size):
-        start += step
         progression.append(start)
+        start += step
 
     correct_answer = progression[correct]
     progression[correct] = '..'
